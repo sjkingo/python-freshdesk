b640317ffa65d97a89cbce1ac72eb9fb5aa02235
prenit-coverfox <37108931+prenit-coverfox@users.noreply.github.com>
2018-07-09 06:17:43 +0530

    Changes from #20 including v1 deprecation
    * defaults to v2 api. fixed pagination issues
    
    * fixed version error message
    
    * Added error handling for V2 api calls

--- freshdesk/api.py
+++ freshdesk/api.py
@@ -25,5 +25,23 @@ def API(domain, api_key, version=1, **kwargs):
     except AttributeError:
         pass
 
+    if version == 1:
+        deprecation_message = """
+            Freshdesk has deprecated their V1 API from 1st July, 2018.
+            For more info, visit https://support.freshdesk.com/support/solutions/articles/231955-important-deprecation-of-api-v1
+            
+            For more info about freshdesk V2 API, visit https://developers.freshdesk.com/api/
+            
+            Now python-freshdesk library will by default return V2 API client. You need to migrate your project accordingly.
+            
+            
+        """
+        print(deprecation_message)
+        version = 2
+
+    if version != 2:
+        print("Freshdesk V%d API is not released yet. Returning default V2 API client\n" % version)
+
+    version = 2
     client_class = _VERSIONS[version]
     return client_class(domain, api_key, **kwargs)
